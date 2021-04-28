773ac36fc62540208d9b89832e877d09ec1707c4
Michael <michaelgainyo@gmail.com>
2021-04-28 11:31:59 -0400

    Updated node src structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,48 +1,24 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
-import { DOMAIN, getSharedData } from "./utils";
+// import { useEffect, useState } from "react";
+// import {  getSharedData } from "./utils";
 
 import "./App.css";
 
 function App() {
-    const [sharedData, setSharedData] = useState({ isLoaded: false });
+    // const [sharedData, setSharedData] = useState({ isLoaded: false });
 
-    useEffect(() => {
-        getSharedData().then((data) => {
-            console.log(data);
-            setSharedData({ ...data, isLoaded: true });
-        });
-    }, []);
+    // useEffect(() => {
+    //     getSharedData().then((data) => {
+    //         setSharedData({ ...data, isLoaded: true });
+    //     });
+    // }, []);
 
     return (
         <div className="App">
             <header className="App-header">
-                <p>Hello!</p>
-                <Firstname {...sharedData} />
-                <Message {...sharedData} />
+                <p>This is React</p>
             </header>
         </div>
     );
 }
-
-const Firstname = () => {
-    const [firstName, setFirstname] = useState(null);
-
-    useEffect(() => {
-        axios.get(`${DOMAIN}/api/firstname/`).then((res) => {
-            const { data } = res;
-            if (data.first_name) setFirstname(data.first_name);
-        });
-    }, []);
-
-    if (!firstName) return <div>Loading ....</div>;
-
-    return <div>I am {firstName}</div>;
-};
-
-const Message = ({ isLoaded, message }) => {
-    if (!isLoaded) return null;
-    return <div>{message}</div>;
-};
 
 export default App;
